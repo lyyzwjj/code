9f8ceda075e5043abbf7f24465a51167c425a1c7
wjj <wzzst310@163.com>
2019-01-30 18:31:16 +0800

    code

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, current_app
+from flask import Flask, current_app, url_for, redirect
 
 # import demo
 
@@ -8,7 +8,7 @@ from flask import Flask, current_app
 # app = Flask(__name__)
 
 # http://127.0.0.1:5000/static/index.html    http://127.0.0.1:5000/python/index.html   对应static下的静态资源  可以访问静态资源
-app = Flask(__name__, static_url_path="/python",  # 访问静态资源的前缀
+app = Flask(__name__, static_url_path="/static",  # 访问静态资源的前缀
             static_folder="static",
             # 相对路径static 当前模块所在的静态文件目录  也可以写绝对路径/Users/wjj/PycharmProjects/code/static   默认就是static
             template_folder="templates"  # 模板文件目录
@@ -19,25 +19,27 @@ app = Flask(__name__, static_url_path="/python",  # 访问静态资源的前缀
 # app = Flask("abcdefg")
 
 # 配置参数的使用方式
-# 1. 使用配置文件
-# app.config. () # 从操作系统中取参数
+# 1. 从系统变量中加载
+# app.config.from_envvar()  # 从操作系统中取参数
+# 2. 使用配置文件
 # app.config.from_pyfile("config.cfg")  # 从配置文件中一次性读取
 
-# 2. 使用对象配置参数
+# 3. 使用对象配置参数
 class Config(object):
     DEBUG = True
     ITCAST = "python"
 
 
-app.config.from_object(Config)
+# app.config.from_object(Config)
 
 
-# 3.直接操作app.config
+# 4.直接操作app.config
 # app.config['DEBUG'] = True
 
 # flask 1.0.2 不能使用config加上'DEBUG'=True 开启debug模式
 
 @app.route('/')
+@app.route('/default')
 def hello_world():
     # i = 1 / 0
     print(app.config.get('ITCAST'))
@@ -46,6 +48,37 @@ def hello_world():
     return 'Hello World!'
 
 
+# @app.route("/post_only", methods=["POST"])
+@app.route("/post_only", methods=["GET", "POST"])
+def post_only():
+    return "post only page"
+
+
+@app.route("/hello", methods=["POST"])
+def hello1():
+    return "hello 1"
+
+
+@app.route("/hello", methods=["GET"])
+def hello2():
+    return "hello 2"
+
+
+@app.route("/login")
+def login():
+    # 使用url_for的函数，通过视图函数的名字找到视图对应的url路径
+    # 函数反推
+    url = url_for("hello_world")
+    print(url)
+    return redirect(url)
+
+
 if __name__ == '__main__':
+    # 查看整个flask中的路由信息
+    print(app.url_map)
+    # app.run()
     # app.run(host="192.168.2.1", port=8080)
-    app.run(host="127.0.0.1", port=8080)
+    # 5.直接设置app
+    # app.debug = True
+    # 6.启动项里面那选择
+    app.run(host="127.0.0.1", port=8080, debug=True)
---
+++ converter_demo.py
@@ -0,0 +1,63 @@
+from flask import Flask, current_app, url_for, redirect
+from werkzeug.routing import BaseConverter
+
+app = Flask(__name__)
+
+
+# 转换器语法  int float path
+# @app.route("/goods/<int:goods_id>")   # int
+@app.route("/goods/<goods_id>")  # 普通字符串 除了"/"
+def goods_detail(goods_id):
+    return "goods detail page %s" % goods_id
+
+
+# 1.定义自己的转换器
+# 万能转换器
+class RegexConverter(BaseConverter):
+    """"""
+
+    # 整个app路由映射列表url_map
+    # 自定义的regex
+    def __init__(self, url_map, regex):
+        # 调用父类的初始化方法
+        super(RegexConverter, self).__init__(url_map)
+        # 将正则表达式的参数保存到对象属性中，flask回去使用这个属性来进行路由的正则匹配
+        self.regex = regex
+
+
+# 2.将自定义的转换器添加到flask的应用中
+app.url_map.converters["re"] = RegexConverter
+
+
+# 3.使用
+
+# 手机号码转换器
+class MobileConverter(BaseConverter):
+    def __init__(self, url_map):
+        # 调用父类的初始化方法
+        super(MobileConverter, self).__init__(url_map)
+        self.regex = r'1[34578]\d{9}'
+
+
+app.url_map.converters["mobile"] = MobileConverter
+
+
+# @app.route("/send/<re(r'1[34578]\d{9}'):mobile>")  # 普通字符串 除了"/"
+@app.route("/send/<mobile:mobile>")  # 普通字符串 除了"/"
+def send_sms(mobile):
+    return "send sms to %s" % mobile
+
+
+@app.route("/call/<re(r'1[34578]\d{9}'):tel>")
+def call_tel(tel):
+    pass
+
+if __name__ == '__main__':
+    # 查看整个flask中的路由信息
+    print(app.url_map)
+    # app.run()
+    # app.run(host="192.168.2.1", port=8080)
+    # 5.直接设置app
+    # app.debug = True
+    # 6.启动项里面那选择
+    app.run(host="127.0.0.1", port=8080, debug=True)
